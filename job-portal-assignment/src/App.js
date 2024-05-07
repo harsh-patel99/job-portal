8d2d2a65adb822b7527bcd6512c5c258684485a9
harsh patel <harsh48703@gmail.com>
2024-05-07 15:04:28 +0530

    init

--- job-portal-assignment/src/App.js
+++ job-portal-assignment/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Assignment Start
+      <header className="App-header">
+        Assignment Initialize
+      </header>
     </div>
   );
 }
